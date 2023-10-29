d99096661ae78b67e6f61663039a0e74d9104dab
TimothyVermeiren <timothy.vermeiren@gmail.com>
2023-10-29 15:29:07 +0100

    Solar modbus update

--- solar_modbus.py
+++ solar_modbus.py
@@ -1,14 +1,55 @@
 from sun2000_modbus import inverter
 from sun2000_modbus import registers
+from pymodbus.exceptions import ConnectionException
+
+import datetime, os, sys, time, traceback
+import psycopg2
 from dotenv import load_dotenv
+
+import postgres
 
 load_dotenv()
 
-inverter_ip = "192.168.200.1"
-inverter_port = 6607
+pg_connection = postgres.connect()
 
-inverter = inverter.Sun2000(host=inverter_ip, port=inverter_port)
-inverter.connect()
-if inverter.isConnected():
-    input_power = inverter.read_formatted(registers.InverterEquipmentRegister.InputPower)
-    print(input_power)
+try:
+    inverter = inverter.Sun2000(host=os.environ["inverter_ip"], port=os.environ["inverter_port"])
+    inverter.connect()
+    if not inverter.isConnected():
+        raise Exception("Could not connect to solar inverter modbus, for... reasons.")
+    print("Successfully connected to solar inverter modbus.")
+except Exception as e:
+    print(f"Unable to connect to solar inverter modbus:\n\t{e}\n\t{traceback.format_exc()}")
+    sys.exit()
+
+while True:
+
+    print("-")
+    print(f"{ datetime.datetime.utcnow() }")
+        
+    try:
+
+        input_power = inverter.read_formatted(registers.InverterEquipmentRegister.InputPower)
+
+        if input_power is not None:
+
+            data_point_value = float(input_power.split(" ")[0])
+            data_point_unit = input_power.split(" ")[1]
+            print(f"Input power: { input_power }")
+
+            # Write to Postgres
+            postgres.log_data(pg_connection, table=os.environ["pg_raw_table"], source="solar_modbus", metric="Input Power", value=data_point_value, unit=data_point_unit)
+        else:
+            print(f"No data read.")
+
+    except Exception as e:
+
+        print(f"Unable to read solar inverter modbus data:\n\t{e}\n\t{traceback.format_exc()}")
+
+        # Could be that we were asking too much. Wait a bit, reconnect, continue.
+        if type(e) == ConnectionException:
+            print("Seems like this was due to our connection being worn out. Let's wait a few seconds and reconnect.")
+            time.sleep(5)
+            inverter.connect()
+
+    time.sleep(2)
