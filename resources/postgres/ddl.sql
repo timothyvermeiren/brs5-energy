c5fd0a8b4a330b9af7206621ab5ed9654bda3d46
TimothyVermeiren <timothy.vermeiren@gmail.com>
2023-12-24 08:20:02 +0000

    Gas Consumption View

--- resources/postgres/ddl.sql
+++ resources/postgres/ddl.sql
@@ -16,3 +16,32 @@ CREATE TABLE solar_forecast (
     value NUMERIC,
     unit VARCHAR(255)
 );
+
+-- DSMR: Gas measurements are only performed every so often, and only show the cumulative total, so we "translate" that here. 
+CREATE VIEW er_gas_consumption AS
+WITH er_gas_consumption AS (
+	SELECT
+		MIN(record_timestamp) AS record_timestamp,
+		"source",
+		metric,
+		value,
+		unit
+	FROM public.energy_raw er
+	WHERE unit = 'm3'
+	GROUP BY "source", metric, value, unit 
+	ORDER BY MIN(record_timestamp) ASC
+)
+SELECT
+	record_timestamp,
+	LAG(record_timestamp) OVER time_window AS previous_timestamp,
+	record_timestamp - LAG(record_timestamp) OVER time_window AS time_interval,
+	EXTRACT(epoch FROM record_timestamp - LAG(record_timestamp) OVER time_window) AS time_seconds,
+	"source",
+	metric,
+	value,
+	LAG(value) OVER time_window AS previous_value,
+	value - LAG(value) OVER time_window AS total_consumption,
+	(value - LAG(value) OVER time_window) / (EXTRACT(epoch FROM record_timestamp - LAG(record_timestamp) OVER time_window) / 3600) AS consumption_m3_per_h, 
+	unit
+FROM er_gas_consumption
+WINDOW time_window AS (ORDER BY record_timestamp)
